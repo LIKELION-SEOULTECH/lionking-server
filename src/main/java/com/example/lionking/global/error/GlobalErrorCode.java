801391bc0bd4014d207cd6fbe0e751633fe85f65
woogie01 <kwonhu3973@gmail.com>
2025-05-05 22:18:28 +0900

    [Feat] implement ErrorCode

--- src/main/java/com/example/lionking/global/error/GlobalErrorCode.java
+++ src/main/java/com/example/lionking/global/error/GlobalErrorCode.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum GlobalErrorCode {
+public enum GlobalErrorCode implements ErrorCode {
+
     /**
      * 200 : 요청 성공
      */
@@ -23,7 +24,7 @@ public enum GlobalErrorCode {
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "REQUEST_001", "잘못된 요청입니다."),
     INVALID_ENUM(HttpStatus.BAD_REQUEST, "REQUEST_002", "Enum 타입으로 변경할 수 없습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "REQUEST_003", "자격 증명이 이루어지지 않았습니다."),
-    INVALID_PERMISSION(HttpStatus.FORBIDDEN,"REQUEST_004","접근 권한이 없습니다."),
+    INVALID_PERMISSION(HttpStatus.FORBIDDEN, "REQUEST_004", "접근 권한이 없습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "REQUEST_005", "잘못된 접근입니다."),
     REQUEST_TIMEOUT(HttpStatus.REQUEST_TIMEOUT, "REQUEST_006", "만료된 접근입니다."),
     NOT_SUPPORTED_URI(HttpStatus.NOT_FOUND, "REQUEST_007", "지원하지 않는 URL입니다."),
@@ -38,7 +39,8 @@ public enum GlobalErrorCode {
      */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "RESPONSE_001", "서버와의 연결에 실패했습니다."),
     BAD_GATEWAY(HttpStatus.BAD_GATEWAY, "RESPONSE_002", "다른 서버로부터 잘못된 응답이 수신되었습니다."),
-    INSUFFICIENT_STORAGE(HttpStatus.INSUFFICIENT_STORAGE, "RESPONSE_003", "서버의 용량이 부족해 요청에 실패했습니다.");
+    INSUFFICIENT_STORAGE(HttpStatus.INSUFFICIENT_STORAGE, "RESPONSE_003", "서버의 용량이 부족해 요청에 실패했습니다."),
+    ;
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/example/lionking/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/lionking/global/error/GlobalExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Slf4j
