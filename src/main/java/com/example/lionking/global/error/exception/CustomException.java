eb03d03f16f441a4b6269311301ab4e0027745c0
woogie01 <kwonhu3973@gmail.com>
2025-05-06 00:29:30 +0900

    [Feat] 초기 프로젝트 설정 마무리

--- src/main/java/com/example/lionking/global/error/exception/CustomException.java
+++ src/main/java/com/example/lionking/global/error/exception/CustomException.java
@@ -5,15 +5,15 @@ import lombok.Getter;
 
 @Getter
 public class CustomException extends RuntimeException {
+
     private final ErrorCode errorCode;
 
     public CustomException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
-    public CustomException(ErrorCode errorCode, String message) {
-        super(message);
+    public CustomException(Throwable cause, ErrorCode errorCode) {
+        super(errorCode.getMessage(), cause);
         this.errorCode = errorCode;
     }
 }
--- src/main/java/com/example/lionking/global/error/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/lionking/global/error/exception/CustomExceptionHandler.java
@@ -2,27 +2,28 @@ package com.example.lionking.global.error.exception;
 
 
 import com.example.lionking.global.error.GlobalErrorCode;
-import com.example.lionking.global.response.ApiResponse;
+import com.example.lionking.global.error.exception.CustomException;
+import com.example.lionking.global.response.ErrorResponse;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
 
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ApiResponse> handleBusinessException(CustomException e) {
-        log.error("customException : {}", e.getErrorCode().getMessage(), e);
-        return ResponseEntity
-                .status(e.getErrorCode().getStatus())
-                .body(ApiResponse.fail(e.getErrorCode()));
+    public ResponseEntity<ErrorResponse> handleBusinessException(CustomException e) {
+        log.error("CustomException : {}", e.getErrorCode().getMessage(), e);
+        return ErrorResponse.createErrorResponseEntity(e.getErrorCode());
     }
 
     @ExceptionHandler({
@@ -32,27 +33,26 @@ public class CustomExceptionHandler {
             InvalidFormatException.class,
             ServletRequestBindingException.class
     })
-    public ResponseEntity<ApiResponse> handleBadRequestException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleBadRequestException(Exception e) {
         log.error("Exception : {}", e.getMessage(), e);
-        return ResponseEntity
-                .status(GlobalErrorCode.BAD_REQUEST.getStatus())
-                .body(ApiResponse.fail(GlobalErrorCode.BAD_REQUEST));
+        return ErrorResponse.createErrorResponseEntity(GlobalErrorCode.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
-    public ResponseEntity<ApiResponse> handleNotFoundException(Exception e) {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(Exception e) {
         log.error("NotFoundException : {}", e.getMessage(), e);
-        return ResponseEntity
-                .status(GlobalErrorCode.NOT_FOUND.getStatus())
-                .body(ApiResponse.fail(GlobalErrorCode.NOT_FOUND));
+        return ErrorResponse.createErrorResponseEntity(GlobalErrorCode.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        log.error("NoHandlerFoundException : {}", e.getMessage(), e);
+        return ErrorResponse.createErrorResponseEntity(GlobalErrorCode.NOT_SUPPORTED_URI);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse> handleException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
         log.error("Exception : {}", e.getMessage(), e);
-        return ResponseEntity
-                .status(GlobalErrorCode.INTERNAL_SERVER_ERROR.getStatus())
-                .body(ApiResponse.fail(GlobalErrorCode.INTERNAL_SERVER_ERROR));
+        return ErrorResponse.createErrorResponseEntity(GlobalErrorCode.INTERNAL_SERVER_ERROR);
     }
-
 }
