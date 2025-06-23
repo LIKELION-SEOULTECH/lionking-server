9a8223f934e5633d532839b9832551d1ac4f514e
woogie01 <kwonhu3973@gmail.com>
2025-06-23 11:38:22 +0900

    [Feat] #13 : Swagger 토큰 설정 추가

--- src/main/java/com/example/lionking/global/config/SwaggerConfig.java
+++ src/main/java/com/example/lionking/global/config/SwaggerConfig.java
@@ -3,15 +3,23 @@ package com.example.lionking.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "JWT";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, jwtSecurityScheme())
+                )
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .info(apiInfo());
     }
 
@@ -20,5 +28,14 @@ public class SwaggerConfig {
                 .title("LikeLion Swagger")
                 .description("홈페이지 프로젝트 API 테스트")
                 .version("1.0.0");
+    }
+
+    private SecurityScheme jwtSecurityScheme() {
+        return new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
     }
 }
