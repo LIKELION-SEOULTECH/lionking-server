df34f1b5de64a44c334edef3190f2c37f8c6747f
woogie01 <kwonhu3973@gmail.com>
2025-06-24 10:00:46 +0900

    [Feat] #15 : 스웨거 태그 변경

--- src/main/java/com/example/lionking/domain/notice/controller/NoticeController.java
+++ src/main/java/com/example/lionking/domain/notice/controller/NoticeController.java
@@ -14,20 +14,20 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/notice")
-@Tag(name = "Notice API", description = "공지사항 등록 및 관리")
+@Tag(name = "Notice API", description = "공지사항 조회")
 public class NoticeController {
 
     private final NoticeService noticeService;
 
     @GetMapping
-    @Operation(summary = "모든 공지 목록 조회", description = "등록된 모든 공지사항을 조회합니다.")
+    @Operation(summary = "모든 공지 목록 조회", description = "등록된 모든 공지사항 조회")
     public ApiResponse<List<NoticeResponse>> getAllNotices() {
         List<NoticeResponse> responses = noticeService.getAllNotices();
         return ApiResponse.success(responses, "공지사항 목록 조회 성공");
     }
 
     @GetMapping("/{noticeId}")
-    @Operation(summary = "공지 상세 조회", description = "특정 공지사항을 조회합니다.")
+    @Operation(summary = "공지 상세 조회", description = "Id에 해당하는 공지사항 상세정보 조회")
     public ApiResponse<NoticeResponse> getNotice(@PathVariable Long noticeId) {
         NoticeResponse response = noticeService.getNotice(noticeId);
         return ApiResponse.success(response, "공지사항 조회 성공");
