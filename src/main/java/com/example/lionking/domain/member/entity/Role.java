58dbf691df6c424c4424db5734f523ee23181980
woogie01 <kwonhu3973@gmail.com>
2025-06-27 22:40:39 +0900

    [Feat] #20 : 게스트 권한 추가

--- src/main/java/com/example/lionking/domain/member/entity/Role.java
+++ src/main/java/com/example/lionking/domain/member/entity/Role.java
@@ -10,6 +10,7 @@ public enum Role {
     MANAGER("ROLE_MANAGER", "운영진"),
     USER("ROLE_USER", "아기사자"),
     PREVIOUS("ROLE_PREVIOUS", "휴면사자"),
+    GUEST("ROLE_GUEST", "게스트")
     ;
 
     private final String authority;
