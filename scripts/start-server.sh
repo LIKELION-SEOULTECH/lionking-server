8801f8544cce998e109dfeed42e1f8fc19b63cd3
choikang <choikang7157@naver.com>
2025-06-28 22:54:29 +0900

    ecr 코드 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop lionking-server || true
 docker rm lionking-server || true
-docker pull {ECR Repository 주소}/lionking-server:latest
-docker run -d --name lionking-server -p 8080:8080 {ECR Repository 주소}/lionking-server:latest
+docker pull 222634398055.dkr.ecr.ap-northeast-2.amazonaws.com/lionking-server:latest
+docker run -d --name lionking-server -p 8080:8080 222634398055.dkr.ecr.ap-northeast-2.amazonaws.com/lionking-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
