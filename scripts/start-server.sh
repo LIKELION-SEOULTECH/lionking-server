2cee3b1b26c67d625aaf5fc9a679c89bda4d4462
choikang <choikang7157@naver.com>
2025-06-28 22:25:00 +0900

    gitignore yml 설정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,5 +4,5 @@ echo "--------------- 서버 배포 시작 -----------------"
 docker stop lionking-server || true
 docker rm lionking-server || true
 docker pull {ECR Repository 주소}/lionking-server:latest
-docker run -d --name instagram-server -p 8080:8080 {ECR Repository 주소}/lionking-server:latest
+docker run -d --name lionking-server -p 8080:8080 {ECR Repository 주소}/lionking-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
